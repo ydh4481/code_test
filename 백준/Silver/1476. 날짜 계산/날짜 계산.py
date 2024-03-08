0a5b234c39cffcc7f94e79ad443c48a06484fd69
ydh4481 <49059590+ydh4481@users.noreply.github.com>
2024-03-08 23:07:12 +0900

    [Silver V] Title: 날짜 계산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.py
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -7,20 +7,9 @@ def add_year(e, s, m):
 
 year = 1
 e_, s_, m_ = 1, 1, 1
-while True:
-    if e_ != e:
-        e_, s_, m_ = add_year(e_, s_, m_)
-        year += 1
-        continue
-    elif s_ != s:
-        e_, s_, m_ = add_year(e_, s_, m_)
-        year += 1
-        continue
-    elif m_ != m:
-        e_, s_, m_ = add_year(e_, s_, m_)
-        year += 1
-        continue
 
-    break
+while (e_, s_, m_) != (e, s, m):
+    e_, s_, m_ = add_year(e_, s_, m_)
+    year += 1
 
 print(year)
