416a00de3561bcdc4ca22c541abbe12a475764bf
양동훈 <49059590+ydh4481@users.noreply.github.com>
2024-06-08 00:06:14 +0900

    [level 2] Title: 타겟 넘버, Time: 3.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,12 +2,12 @@ def solution(numbers, target):
     answer = 0
     
     def dfs(idx=0, total=0, answer=0):
-        if idx == len(numbers):
-            if total == target:
-                answer += 1
-            return answer
-        answer = dfs(idx + 1, total + numbers[idx], answer)
-        answer = dfs(idx + 1, total - numbers[idx], answer)
+        if idx == len(numbers): # 정수를 다 썼으면,
+            if total == target:     # 타겟 값과 토탈 값이 동일하면,
+                answer += 1             # 정답
+            return answer           # 리턴
+        answer = dfs(idx + 1, total + numbers[idx], answer) # 다음 정수를 더함
+        answer = dfs(idx + 1, total - numbers[idx], answer) # 다음 정수를 뺌
         return answer
     
     answer = dfs()
