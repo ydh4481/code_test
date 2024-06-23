8276a73cc0f3a53467d79408fbc7303484e66c54
양동훈 <49059590+ydh4481@users.noreply.github.com>
2024-06-23 15:06:37 +0900

    Time: 459 ms (87.09%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,23 @@
+-- Write a solution to report the sum of all total investment values in 2016 tiv_2016, for all policyholders who:
+
+-- have the same tiv_2015 value as one or more other policyholders, and
+-- are not located in the same city as any other policyholder (i.e., the (lat, lon) attribute pairs must be unique).
+-- Round tiv_2016 to two decimal places.
+
+-- The result format is in the following example.
+
+
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
